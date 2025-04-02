36c2934b2904ed6833fa97e58d9007aea255edb4
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2025-04-02 18:04:50 +1100

    Initial Commit

---
+++ BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by user on 2025/04/02.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubblePopTests/BubblePopTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopTests.swift
+//  BubblePopTests
+//
+//  Created by user on 2025/04/02.
+//
+
+import Testing
+@testable import BubblePop
+
+struct BubblePopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
