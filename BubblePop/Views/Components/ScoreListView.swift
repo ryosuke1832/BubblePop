f7888db8fe6e3bcdb0fe1118d47b90e8e5d841ed
ryosuke1832 <84903480+ryosuke1832@users.noreply.github.com>
2025-04-21 00:12:03 +1000

    0421_Refactaring

---
+++ BubblePop/Views/Components/ScoreListView.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreListView.swift
+//  BubblePop
+//
+//  Created by user on 2025/04/20.
+//
+
+import SwiftUI
+
+struct ScoreListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreListView()
+}
